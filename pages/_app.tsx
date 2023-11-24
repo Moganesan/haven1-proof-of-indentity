48c02b3a4613e97335b21ae85231781e65bdc358
moganesan <developer.moganesan>
2023-11-25 01:35:43 +0530

    root component wrapped with metamaskProvider and Auth0Providers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { MetaMaskUIProvider } from "@metamask/sdk-react-ui";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Auth0Provider
+      domain="dev-83zcj1w1daq5x7sc.us.auth0.com"
+      clientId="wsu0nOFxC161ebKFh18F0qb8mEMZi7dl"
+      authorizationParams={{ redirect_uri: "http://localhost:3000" }}
+    >
+      <MetaMaskUIProvider
+        sdkOptions={{
+          dappMetadata: {
+            name: "Social Hub",
+          },
+        }}
+      >
+        <Component {...pageProps} />
+      </MetaMaskUIProvider>
+    </Auth0Provider>
+  );
 }
